6e33cc34674efd73009b557237658b729b99f052
gbtsui <gabriel.tsui@protonmail.com>
2026-02-03 10:19:24 -0500

    feat: implement zooming
    todo: implement zooming limits, but i don't know if those are actually needed

--- app/components/periodic-table/PeriodicTable.tsx
+++ app/components/periodic-table/PeriodicTable.tsx
@@ -2,7 +2,14 @@
 
 import {periodicTableElementsBasicData} from "@/public/elementData/periodic-table";
 import {ElementBasicMetadata} from "@/app/schema";
-import {MouseEventHandler, MouseEvent, useRef, useState, ReactNode} from "react";
+import {
+    MouseEventHandler,
+    MouseEvent,
+    useRef,
+    useState,
+    ReactNode,
+    WheelEventHandler
+} from "react";
 
 type PeriodicTableProps = {
     visible: boolean;
@@ -16,13 +23,17 @@ type PannableAreaProps = {
     children: ReactNode;
 }
 
-type SillyCoordinates = {x: number, y: number}
+type SillyCoordinates = { x: number, y: number }
 
 function PannableArea(props: PannableAreaProps) {
     const [offset, setOffset] = useState<SillyCoordinates>({x: 0, y: 0});
     const startRef = useRef<SillyCoordinates>({x: 0, y: 0});
     const offsetRef = useRef<SillyCoordinates>({x: 0, y: 0});
-    const [panning, setPanning] = useState(false);
+    const [panning, setPanning] = useState<boolean>(false);
+    const [scale, setScale] = useState<number>(1)
+
+    const zoomOutLimit = 0.2 //how far out can you zoom?
+    const zoomInLimit = 8 //how far in can you zoom?
 
     const onMouseDown: MouseEventHandler<HTMLDivElement> = (event: MouseEvent) => {
         setPanning(true)
@@ -46,23 +57,35 @@ function PannableArea(props: PannableAreaProps) {
         offsetRef.current = offset
     }
 
+    const onWheel: WheelEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault()
+
+        const zoomFactor = 0.001
+        const next = scale - e.deltaY * zoomFactor
+
+        setScale(Math.min(4, Math.max(0.25, next)))
+    }
+
     return (
         <div
             onMouseDown={onMouseDown}
             onMouseMove={onMouseMove}
             onMouseUp={onMouseUp}
             onMouseLeave={onMouseUp}
+            onWheel={onWheel}
             style={{
-                width: "100vw",
-                height: "100vh",
+                width: "98vw",
+                height: "98vh",
                 overflow: "hidden",
                 border: "1px solid black",
                 cursor: panning ? "grabbing" : "grab",
+                backgroundColor: "#161625"
             }}
         >
             <div
                 style={{
-                    transform: `translate(${offset.x}px, ${offset.y}px)`,
+                    transform: `translate(${offset.x}px, ${offset.y}px) scale(${scale}`,
+                    transformOrigin: "0 0"
                     //width:
                 }}
             >
@@ -89,5 +112,9 @@ export default function PeriodicTable(props: PeriodicTableProps) {
     )
     */
 
-    return <PannableArea><div className={"select-none"}>test one two three</div></PannableArea>
+    return (
+        <PannableArea>
+            <div className={"select-none"}>test one two three</div>
+        </PannableArea>
+    )
 }
