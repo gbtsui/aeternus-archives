79660791dad8146cecd0e4e49328efb6db23be76
gbtsui <gabriel.tsui@protonmail.com>
2026-02-03 20:17:58 -0500

    feat: added a lot more character information so my table doesnt look so sad

--- public/elementData/periodic-table.ts
+++ public/elementData/periodic-table.ts
@@ -23,5 +23,124 @@ export const periodicTableElementsBasicData: Record<number, ElementBasicMetadata
         archiveDocuments: []
     },
 
-    
+    11: {
+        atomicNumber: 11,
+        symbol: "Na",
+        name: "Sodium",
+        elementType: "alkali",
+        column: 1,
+        row: 3,
+        characterName: "Sofia",
+        archiveDocuments: []
+    },
+
+    12: {
+        atomicNumber: 12,
+        symbol: "Mg",
+        name: "Magnesium",
+        elementType: "alkalineEarth",
+        column: 2,
+        row: 3,
+        characterName: "Alethée",
+        archiveDocuments: []
+    },
+
+    20: {
+        atomicNumber: 20,
+        symbol: "Ca",
+        name: "Calcium",
+        elementType: "alkalineEarth",
+        column: 2,
+        row: 4,
+        characterName: "Petros",
+        archiveDocuments: []
+    },
+
+    79: {
+        atomicNumber: 79,
+        symbol: "Au",
+        name: "Gold",
+        elementType: "transitionMetal",
+        column: 11,
+        row: 6,
+        characterName: "Aurelius",
+        archiveDocuments: []
+    },
+
+    2: {
+        atomicNumber: 2,
+        symbol: "He",
+        name: "Helium",
+        elementType: "nobleGas",
+        column: 18,
+        row: 1,
+        characterName: "Laplace",
+        archiveDocuments: []
+    },
+
+    7: {
+        atomicNumber: 7,
+        symbol: "N",
+        name: "Nitrogen",
+        elementType: "nonMetal",
+        column: 15,
+        row: 2,
+        characterName: "Qiya",
+        archiveDocuments: []
+    },
+
+    17: {
+        atomicNumber: 17,
+        symbol: "Cl",
+        name: "Chlorine",
+        elementType: "halogen",
+        column: 17,
+        row: 3,
+        characterName: "Kloros",
+        archiveDocuments: []
+    },
+
+    53: {
+        atomicNumber: 53,
+        symbol: "I",
+        name: "Iodine",
+        elementType: "halogen",
+        column: 17,
+        row: 5,
+        characterName: "Astatine",
+        archiveDocuments: []
+    },
+
+    15: {
+        atomicNumber: 15,
+        symbol: "P",
+        name: "Phosphorous",
+        elementType: "nonMetal",
+        column: 15,
+        row: 3,
+        characterName: "Ambrose",
+        archiveDocuments: []
+    },
+
+    74: {
+        atomicNumber: 74,
+        symbol: "W",
+        name: "Tungsten",
+        elementType: "transitionMetal",
+        column: 6,
+        row: 6,
+        characterName: "Andrzej",
+        archiveDocuments: []
+    },
+
+    19: {
+        atomicNumber: 19,
+        symbol: "K",
+        name: "Potassium",
+        elementType: "alkali",
+        column: 1,
+        row: 4,
+        characterName: "Ashe",
+        archiveDocuments: []
+    }
 }
