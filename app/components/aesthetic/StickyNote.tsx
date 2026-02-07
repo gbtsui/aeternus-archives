c4e1c8bca5e34e3643c4d845f286f4cfe5c656b8
gbtsui <gabriel.tsui@protonmail.com>
2026-02-06 20:10:45 -0500

    reformat: make it more readable for later

--- app/components/aesthetic/StickyNote.tsx
+++ app/components/aesthetic/StickyNote.tsx
@@ -16,10 +16,13 @@ export default function StickyNote(props: StickyNoteProps) {
     const [mounted, setMounted] = useState(true);
 
     useEffect(() => {
-        const funny_async_function = async () =>  {if (!props.tilt) setTilt(Math.round((Math.random() - 0.5) * 20))}
+        const funny_async_function = async () => {
+            if (!props.tilt) setTilt(Math.round((Math.random() - 0.5) * 20))
+        }
 
         funny_async_function();
-        return () => {}
+        return () => {
+        }
     }, [setTilt, props.tilt]);
 
     const harryStyles: CSSProperties = {
@@ -32,12 +35,10 @@ export default function StickyNote(props: StickyNoteProps) {
     if (!mounted) return null;
 
     return (
-        <div className={`absolute font-nothing-you-could-do bg-amber-200 text-blue-900 w-[200px] h-[200px] text-center justify-center p-[25px] flex flex-col cursor-pointer pointer-events-auto z-10 ${visible ? "" : "exit"} `} style={harryStyles} onClick={() => setVisible(false)} onAnimationEnd={() => setMounted(false)}>
-
+        <div
+            className={`absolute font-nothing-you-could-do bg-amber-200 text-blue-900 w-[200px] h-[200px] text-center justify-center p-[25px] flex flex-col cursor-pointer pointer-events-auto z-10 ${visible ? "" : "exit"} `}
+            style={harryStyles} onClick={() => setVisible(false)} onAnimationEnd={() => setMounted(false)}>
             {children}
-            <div className={"cursor-pointer"}>
-                {tilt}deg
-            </div>
         </div>
     )
 }
